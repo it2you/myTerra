b8a940ab1d3ce2bfe82f80a9d420382f31cbfdad
root <root@doctl.lxd>
2025-08-26 11:33:55 +0000

    :q

--- ecs.tf
+++ ecs.tf
@@ -37,7 +37,7 @@ resource "aws_ecs_service" "ecs_td" {
 	name            = "${var.prefix}-app-service"
 	cluster         = aws_ecs_cluster.ecs_cluster.id
 	task_definition = aws_ecs_task_definition.ecs_td.arn
-	desired_count   = 1
+	desired_count   = 2
 	launch_type     = "FARGATE"
 	network_configuration {
 		subnets          = [aws_subnet.public_subnet_a.id]
